dd8ea7e1168f6b00ae23f4a1ad47d6ca4f1eef44
Mubashir Ghani Shah <mubashirghanishah720@gmail.com>
2026-06-13 21:12:14 +0500

    Day 2 Added

---
+++ Day_2.py
@@ -0,0 +1,16 @@
+# bolean True or false
+employee = True
+if employee:
+    print("CUrrently employeed")
+else:
+    print("Deasesed")
+# Type Casting -> process -> converting variable from one datatype from another
+    student_name = "Mubashir"
+    ID_no = 1280
+    GPA = 3.79
+    Inrolled = True
+    print(type(student_name))
+    print(type(ID_no))
+    print(type(GPA))
+    print(type(Inrolled))
+    print("HI Pyth")
