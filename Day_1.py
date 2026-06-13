2e60b8f7e08bd93582bee4f4ddf6969c40dc99f7
Mubashir Ghani Shah <mubashirghanishah720@gmail.com>
2026-06-13 16:36:19 +0500

    first python

---
+++ Day_1.py
@@ -0,0 +1,27 @@
+#Print
+print("Mubashir")
+#print("Khan")
+#Variables
+#String
+name= "Mubashir"
+print(name)
+Class = "BSCS"
+print(Class)
+#Integer
+Age = 20
+print (Age)
+Roll_No = 909556
+print(Roll_No)
+#Float
+GPA = 3.79
+print(GPA)
+AV_Marks = 85.6
+print(AV_Marks)
+# f_Command
+print("-----F_Command-----")
+print(f"My name is {name}")
+print(f"My Age is {Age}")
+print(f"I read in {Class}")
+print(f"My Roll no is {Roll_No}")
+print(f"My Total GPA Out Of 4 Is {GPA}")
+print(f"My Average marks of Whole Papers is {AV_Marks}")
---
+++ main.py
@@ -0,0 +1,6 @@
+a=-43
+b=43.6
+c=23j
+print(type(a))
+print(type(b))
+print(type(c))
