765db3529c7d0298da9c8babd34adc794ec72116
Mubashir Ghani Shah <mubashirghanishah720@gmail.com>
2026-06-13 21:59:53 +0500

    Day_2 Completed

--- Day_2.py
+++ Day_2.py
@@ -4,13 +4,19 @@ if employee:
     print("CUrrently employeed")
 else:
     print("Deasesed")
-# Type Casting -> process -> converting variable from one datatype from another
-    student_name = "Mubashir"
-    ID_no = 1280
-    GPA = 3.79
-    Inrolled = True
-    print(type(student_name))
-    print(type(ID_no))
-    print(type(GPA))
-    print(type(Inrolled))
-    print("HI Pyth")
+#Type Casting process converting type from one to another
+student_name = "Mubashir"
+ID_no = 1280
+GPA = 3.79
+Inrolled = True
+print(type(student_name))   # → <class 'str'>
+print(type(GPA))            # → <class 'float'>
+print(type(ID_no))          # → <class 'int'>
+print(type(Inrolled))       # → <class 'bool'>
+#input take data from user
+name = input(f"What is your name :")
+Age = input(f"What is Your Age : ")
+ID_no = input(f"Your ID is : ")
+GPA = input(f"What GPA You Got : ")
+Inrolled = input(f"IS You Are Inrolled")
+print(f"My name is {name} I am {Age} Years Old I Got {GPA} In 1st semester {Inrolled} i am inrolled")
