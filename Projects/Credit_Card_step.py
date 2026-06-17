a6373dbbffcd35a4382fdfe931d368b437a2635b
Mubashir Ghani Shah <mubashirghanishah720@gmail.com>
2026-06-17 07:27:35 +0500

    Credit Card

---
+++ Projects/Credit_Card_step.py
@@ -0,0 +1,4 @@
+# just show last 4 digits of credit card and hide other digts
+credit_card=input("Enter your credit card number : ")
+last_digit = credit_card[8:12]
+print(f"Your credit card last 4 digit number is : XXXX XXXX {last_digit}")
